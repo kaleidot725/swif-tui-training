fb5dc1c4a9e5bb7208dfc610e0c6735896229030
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-08-31 20:59:49 +0900

    Create SwiftUIHierachyr

---
+++ projects/SwiftUIHierarchyr/SwiftUIHierarchyr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIHierarchyr
+//
+//  Created by 桂川 祐介 on 2023/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projects/SwiftUIHierarchyr/SwiftUIHierarchyr/SwiftUIHierarchyrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHierarchyrApp.swift
+//  SwiftUIHierarchyr
+//
+//  Created by 桂川 祐介 on 2023/08/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHierarchyrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
