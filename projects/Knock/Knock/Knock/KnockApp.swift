4a847ec967bbd6117cb0b6e2f8050f03ea865d72
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-09-18 20:58:37 +0900

    Create project and modify directory

---
+++ projects/Knock/Knock/Knock/KnockApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnockApp.swift
+//  Knock
+//
+//  Created by 桂川 祐介 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct KnockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
