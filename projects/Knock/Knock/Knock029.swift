34e619b4fbfd2f9fff092d7599cfd861bfddddce
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-11-16 21:03:11 +0900

    Update

--- projects/Knock/Knock/Knock029.swift
+++ projects/Knock/Knock/Knock029.swift
@@ -10,17 +10,21 @@ struct Knock029: View {
     @State var selectedNumber2 = "0"
 
     var body: some View {
-        HStack {
-            Picker("SelectedNumber1", selection: $selectedNumber1) {
-                ForEach(0 ..< 24) { index in
-                    Text("\(index)").tag("\(index)")
-                }
-            }.pickerStyle(.inline)
-            Picker("SelectedNumber2", selection: $selectedNumber2) {
-                ForEach(0 ..< 60) { index in
-                    Text("\(index)").tag("\(index)")
-                }
-            }.pickerStyle(.inline)
+        VStack {
+            Text(selectedNumber1)
+            Text(selectedNumber2)
+            HStack {
+                Picker("SelectedNumber1", selection: $selectedNumber1) {
+                    ForEach(0 ..< 24) { index in
+                        Text("\(index)").tag("\(index)")
+                    }
+                }.pickerStyle(.inline)
+                Picker("SelectedNumber2", selection: $selectedNumber2) {
+                    ForEach(0 ..< 60) { index in
+                        Text("\(index)").tag("\(index)")
+                    }
+                }.pickerStyle(.inline)
+            }
         }
     }
 }
